a1c087ad2df0e7ffdf6848453fed3a729c84737e
nathanfaucett <nathanfaucett@gmail.com>
2014-11-07 12:48:01 -0600

    updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 var methods = require("methods"),
     each = require("each"),
     type = require("type"),
-    utils = require("utils"),
 
     defaults = require("./defaults"),
     request = process.browser ? require("./request_browser") : require("./request_node");
@@ -10,46 +9,27 @@ var methods = require("methods"),
 each(methods, function(method) {
     var upper = method.toUpperCase();
 
-    request[method] = function(url, options) {
+    request[method] = function(url, data, options) {
+        if (options === undefined && type.isObject(data)) {
+            options = data;
+            data = options.data;
+        }
         if (type.isObject(url)) {
             options = url;
+            data = options.data;
             url = options.url || options.src;
         }
 
         options || (options = {});
 
         options.url = url;
+        options.data = data;
         options.method = upper;
 
         return request(options);
     };
 });
-
 request.mSearch = request["m-search"];
-
-request.post = function(url, data, options) {
-    return request(utils.mixin({
-        url: url,
-        data: data,
-        method: "POST"
-    }, options));
-};
-
-request.put = function(url, data, options) {
-    return request(utils.mixin({
-        url: url,
-        data: data,
-        method: "PUT"
-    }, options));
-};
-
-request.patch = function(url, data, options) {
-    return request(utils.mixin({
-        url: url,
-        data: data,
-        method: "PATCH"
-    }, options));
-};
 
 request.defaults = defaults.values;
 
